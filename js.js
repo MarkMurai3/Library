a9d467e8701310c80dd7dd81ded5af35f37783cb
Futureland3 <futureland03@gmail.com>
2025-07-03 09:23:33 +0200

    Finish Modal

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
-// const newBook = document.getElementsByClassName("newBook");
-// const popUp = document.getElementsByClassName("popUp");
+
 const popUp = document.getElementById("popUpID");
 const newBookBtn = document.getElementById("newBookbtn");
 const submitBtn = document.getElementById("submitID");
+const closeBtn = document.getElementById("close");
 
 
 newBookBtn.onclick = function (){
@@ -18,4 +18,11 @@ window.onclick = function(event){
         popUp.style.display = "none";
     }
 }
-// console.log(popUp.style.display);
+
+closeBtn.onclick = function(){
+    popUp.style.display = "none";
+}
+
+//When pressing Submit a new Book object will be created with its ID. crypto.randomUUID()
+//The new object will be placed in the Library array
+//A loop that would update the table will run
